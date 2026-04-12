5fc0d7ab8558980759a68185403b6b451fad6f96
gugulothushivani97 <gugulothushivani97@gmail.com>
2026-04-12 11:13:08 +0530

    Delete Smart Multimeter Using Microcontroller Systems/endterm/protocol.py

--- Smart Multimeter Using Microcontroller Systems/endterm/protocol.py
+++
@@ -1,7 +0,0 @@
-def display_result(actual, measured, error):
-    return (
-        f"Input Resistance : {actual} Ω\n"
-        f"Measured Value   : {round(measured, 2)} Ω\n"
-        f"Percentage Error : {round(error, 4)} %\n"
-        "-----------------------------"
-    )
